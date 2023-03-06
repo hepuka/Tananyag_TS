30d63d9767acbd14cc7054d274e3595d738ee18c
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-03-06 23:20:40 +0100

    upd

--- dist/App.js
+++ dist/App.js
@@ -156,6 +156,20 @@ const user5 = {
 };
 const result = addId(user4);
 console.log(result);
+//////////////////////////ENUMS///////////////////////
+var StatusEnum;
+(function (StatusEnum) {
+    StatusEnum["NotStarted"] = "notStarted";
+    StatusEnum["InProgress"] = "inProgress";
+    StatusEnum["Done"] = "Done";
+})(StatusEnum || (StatusEnum = {}));
+let notStartedStatus = StatusEnum.NotStarted;
+console.log(notStartedStatus); //0, mivel az első helyen áll az enum-ban
+notStartedStatus = StatusEnum.Done; //felülírom a változó értékét
+console.log(notStartedStatus); //2
+console.log(StatusEnum.NotStarted); //0
+console.log(StatusEnum.InProgress); //1
+console.log(StatusEnum.Done); //2
 //////////////////PUBLIC,PRIVATE,PROTECTED, READONLY////////////
 //PUBLIC alapértelmezett
 //PRIVATE - csak az osztályon belül használható
--- src/App.ts
+++ src/App.ts
@@ -307,6 +307,31 @@ const result = addId<UserInterface3<{}, string>>(user4);
 
 console.log(result);
 
+//////////////////////////ENUMS///////////////////////
+
+enum StatusEnum {
+  NotStarted = "notStarted",
+  InProgress = "inProgress",
+  Done = "Done",
+}
+
+//ENUM használata INTERFACE-ben
+//status változó típusa enum
+interface Tssk {
+  id: string;
+  status: StatusEnum;
+}
+
+let notStartedStatus: StatusEnum = StatusEnum.NotStarted;
+console.log(notStartedStatus); //0, mivel az első helyen áll az enum-ban
+
+notStartedStatus = StatusEnum.Done; //felülírom a változó értékét
+console.log(notStartedStatus); //2
+
+console.log(StatusEnum.NotStarted); //0
+console.log(StatusEnum.InProgress); //1
+console.log(StatusEnum.Done); //2
+
 //////////////////PUBLIC,PRIVATE,PROTECTED, READONLY////////////
 
 //PUBLIC alapértelmezett
