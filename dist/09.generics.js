ed99c102f5979fe315c4d4410ee006ca90b9cafb
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-03-09 21:27:23 +0100

    upd

--- dist/09.generics.js
+++ dist/09.generics.js
@@ -1,5 +1,6 @@
 "use strict";
 //T a default neve a generic-nek
+//generic egy placeholder amibe bármilyen típusú adatot berakhatunk.
 //minden generic adattípust <>-be kell rakni
 const addId = (obj) => {
     const id = Math.random().toString(16); //hexa számot ad vissza
@@ -21,3 +22,123 @@ const user5 = {
 };
 const result = addId(user4);
 console.log(result);
+///////////////////////////////////
+const echo = (arg) => arg;
+const isObj = (arg) => {
+    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+};
+console.log(isObj(true));
+console.log(isObj("John"));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
+console.log(isObj(null));
+//2.pld
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("Dave"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: "Dave" }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+const processUser = (user) => {
+    return user;
+};
+console.log(processUser({ id: 1, name: "Dave" }));
+//5.pld
+const getUserProperty = (users, key) => {
+    return users.map((item) => item[key]);
+};
+const usersArray = [
+    {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496",
+            },
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+            name: "Romaguera-Crona",
+            catchPhrase: "Multi-layered client-server neural-net",
+            bs: "harness real-time e-markets",
+        },
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+        address: {
+            street: "Victor Plains",
+            suite: "Suite 879",
+            city: "Wisokyburgh",
+            zipcode: "90566-7771",
+            geo: {
+                lat: "-43.9509",
+                lng: "-34.4618",
+            },
+        },
+        phone: "010-692-6593 x09125",
+        website: "anastasia.net",
+        company: {
+            name: "Deckow-Crist",
+            catchPhrase: "Proactive didactic contingency",
+            bs: "synergize scalable supply-chains",
+        },
+    },
+];
+console.log(getUserProperty(usersArray, "email"));
+console.log(getUserProperty(usersArray, "username"));
+///////////////////////////////////////
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const store = new StateObject("John");
+console.log(store.state);
+store.state = "Dave";
+//store.state = 12
+const myState = new StateObject([15]);
+myState.state = ["Dave", 42, true];
+console.log(myState.state);
