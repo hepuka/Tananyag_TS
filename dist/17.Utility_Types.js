a95d213fc27d02bea028ed7369f2c4b9303e04f1
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-03-09 21:43:01 +0100

    upd

--- dist/17.Utility_Types.js
+++ dist/17.Utility_Types.js
@@ -1,74 +1,102 @@
 "use strict";
 // Utility Types
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
+  };
 const updateAssignment = (assign, propsToUpdate) => {
-    return Object.assign(Object.assign({}, assign), propsToUpdate);
+  return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
 const assign1 = {
-    studentId: "compsci123",
-    title: "Final Project",
-    grade: 0,
+  studentId: "compsci123",
+  title: "Final Project",
+  grade: 0,
 };
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded = updateAssignment(assign1, { grade: 195 });
 console.log(assignGraded);
 // Required and Readonly
 const recordAssignment = (assign) => {
-    return assign;
+  return assign;
 };
-const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+const assignVerified = Object.assign(Object.assign({}, assignGraded), {
+  verified: true,
+});
 // NOTE: assignVerified won't work with recordAssignment!
 // Why? Try it and see what TS tells you :)
-recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+recordAssignment(
+  Object.assign(Object.assign({}, assignGraded), { verified: true })
+);
 // Record
 const hexColorMap = {
-    red: "FF0000",
-    green: "00FF00",
-    blue: "0000FF",
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF",
 };
 const finalGrades = {
-    Sara: "B",
-    Kelly: "U",
+  Sara: "B",
+  Kelly: "U",
 };
 const gradeData = {
-    Sara: { assign1: 85, assign2: 93 },
-    Kelly: { assign1: 76, assign2: 15 },
+  Sara: { assign1: 85, assign2: 93 },
+  Kelly: { assign1: 76, assign2: 15 },
 };
 const score = {
-    studentId: "k123",
-    grade: 85,
+  studentId: "k123",
+  grade: 85,
 };
 const preview = {
-    studentId: "k123",
-    title: "Final Project",
+  studentId: "k123",
+  title: "Final Project",
 };
 // ReturnType
 //type newAssign = { title: string, points: number }
 const createNewAssign = (title, points) => {
-    return { title, points };
+  return { title, points };
 };
 const tsAssign = createNewAssign("Utility Types", 100);
 console.log(tsAssign);
 const assignArgs = ["Generics", 100];
 const tsAssign2 = createNewAssign(...assignArgs);
 console.log(tsAssign2);
-const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+const fetchUsers = () =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const data = yield fetch("https://jsonplaceholder.typicode.com/users")
-        .then((res) => {
+      .then((res) => {
         return res.json();
-    })
-        .catch((err) => {
-        if (err instanceof Error)
-            console.log(err.message);
-    });
+      })
+      .catch((err) => {
+        if (err instanceof Error) console.log(err.message);
+      });
     return data;
-});
+  });
 fetchUsers().then((users) => console.log(users));
